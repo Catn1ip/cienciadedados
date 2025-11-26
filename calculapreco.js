f80d59cb313e526abaf1a16df43ab6117e26f9b3
Deftones da Silva <161340682+Catn1ip@users.noreply.github.com>
2025-11-26 12:09:04 -0300

    Add price calculation function with logging
    Function to calculate final price with discount and log results.

---
+++ calculapreco.js
@@ -0,0 +1,29 @@
+function calcularPrecoFinal(produto) {
+  const desconto = produto.desconto ?? 0;
+
+ 
+  const precoFinal = produto.preco - (produto.preco * desconto / 100);
+
+  console.log(`Preço inicial de ${produto.nome}: R$ ${produto.preco.toFixed(2)}`);
+  console.log(`Preço final de ${produto.nome}: R$ ${precoFinal.toFixed(2)}`);
+
+}
+
+// Exemplo de uso
+
+const produto1 = {
+
+  nome: "Fone de ouvido",
+
+  preco: 200,
+
+  desconto: 79
+
+};
+
+calcularPrecoFinal(produto1);
+
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Try programiz.pro");
